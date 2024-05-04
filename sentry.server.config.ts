a1d11193d1ec2fbe8764ba368c9a05aef7c3d17a
Martin Winther <martin.winther@protonmail.com>
2024-05-04 21:15:28 +0200

    add modal

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://154c374e8146c3480081b65115fc37cf@o4507199448219648.ingest.de.sentry.io/4507199458050128",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
---
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -0,0 +1,14 @@
+import { FullPageImageView } from "~/app/common/full-page-image-view";
+import { Modal } from "./modal";
+
+export default async function PhotoModal({
+  params: { id: photoId },
+}: {
+  params: { id: string };
+}) {
+  return (
+    <Modal>
+      <FullPageImageView photoId={photoId} />
+    </Modal>
+  );
+}
